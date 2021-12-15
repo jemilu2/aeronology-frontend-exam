b677a4a0b4d9ee81a7cd91f941d9c98ba0c29166
jemilu <jemilu@crowncitytechnologies.com>
2021-12-15 17:10:56 +0000

    cleaned up todo slice

---
+++ src/features/todo/todoApi.ts
@@ -0,0 +1,8 @@
+import { iTodo } from "./todoSlice";
+
+// A mock function to mimic making an async request for data
+export function addTodoAsync(todo: iTodo) {
+  return new Promise<{ data: iTodo }>((resolve) =>
+    setTimeout(() => resolve({ data: todo }), 500)
+  );
+}
--- src/features/todo/todoSlice.ts
+++ src/features/todo/todoSlice.ts
@@ -6,24 +6,20 @@ export interface iTodo {
     title: string;
     done: boolean;
     deleted: boolean;
-  }
+}
 
 export type TodoFilter = "ALL" | "DONE" | "DELETED"
 
 export interface iTodoState {
     todos: iTodo[];
     filter: TodoFilter;
+    status: "loading" | "idle" | "error";
 }
 
 const initialState: iTodoState = {
-    todos: [
-        { id: 1, title: "Type that letter", done: false, deleted: false },
-        { id: 2, title: "Call cupcake", done: true, deleted: false },
-        { id: 3, title: "Make that prototype", done: false, deleted: false },
-        { id: 4, title: "Call the police", done: false, deleted: false },
-    ],
-    filter: "ALL"
-    // TODO: wipe those defaults out
+    todos: [],
+    filter: "ALL",
+    status: "idle"
 }
 
 export const todoSlice = createSlice({
@@ -33,23 +29,23 @@ export const todoSlice = createSlice({
         addTodo: (state: iTodoState, action: PayloadAction<iTodo>) => {
             state.todos.push(action.payload)
         },
-        deleteTodo: (state: iTodoState, action: PayloadAction<iTodo>) =>  {
+        deleteTodo: (state: iTodoState, action: PayloadAction<iTodo>) => {
             const index = state.todos.map((todo: iTodo) => todo.id).indexOf(action.payload.id)
-            if(index !== -1) {
+            if (index !== -1) {
                 state.todos.splice(index, 1)
             }
         },
-        completeTodo: (state: iTodoState, action: PayloadAction<iTodo>) =>  {
+        completeTodo: (state: iTodoState, action: PayloadAction<iTodo>) => {
             const { payload: todo } = action;
             const index = state.todos.map((todo: iTodo) => todo.id).indexOf(action.payload.id)
-            if(index !== -1) {
+            if (index !== -1) {
                 state.todos.splice(index, 1, { ...todo, done: true })
             }
         },
-        editTodo: (state: iTodoState, action: PayloadAction<iTodo>) =>  {
+        editTodo: (state: iTodoState, action: PayloadAction<iTodo>) => {
             const { payload: todo } = action;
             const index = state.todos.map((todo: iTodo) => todo.id).indexOf(todo.id)
-            if(index !== -1) {
+            if (index !== -1) {
                 state.todos.splice(index, 1, todo)
             }
         }
