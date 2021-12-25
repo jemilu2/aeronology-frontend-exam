22ad989e99fff5e8b3b7fa87ab9e55c0e40f82ef
Jemilu Mohammed <mohammedjemilu@gmail.com>
2021-12-25 15:10:40 +0000

    edit todo item to use useDispatch
    we were managing a bit too much state
    
    I'm beginning to wonder if it will be cool to always refactor components to use useDispatch after we make an arbitrary x number of useState calls

--- src/TodoItem.tsx
+++ src/TodoItem.tsx
@@ -7,7 +7,7 @@ import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { deleteTodo, editTodo, iTodo } from "./features/todo/todoSlice";
 import { useAppDispatch } from "./app/hooks";
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import { useMutation } from "./features/todo/useMutation";
 import { makeCrudRequest as editTodoAsync } from "./features/todo/todoApi";
 import LoadingButton from "@mui/lab/LoadingButton";
@@ -15,50 +15,72 @@ import SaveIcon from "@mui/icons-material/Save"
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
 
+export interface iTodoItemState {
+  editing: boolean,
+  editingTitle: string;
+  showSuccessSnackBar: boolean;
+  showFailureSnackBar: boolean;
+}
 
+export interface iTodoItemAction {
+  type: string;
+  payload?: any;
+}
+
+const initialState = {
+  editing: false,
+  editingTitle: "",
+  showSuccessSnackBar: false,
+  showFailureSnackBar: false
+};
+
+export function reducer(state: iTodoItemState, action: iTodoItemAction) {
+  const { payload } = action;
+  switch (action.type) {
+    case 'SET_EDITING_TODO_TITLE':
+      return { ...state, editingTitle: payload }
+    case 'EDIT_TODO':
+      return {...state, editingTitle: payload, editing: true };
+    case 'CANCEL_EDITING':
+      return { ...state, editing: false, editingTitle: "" };
+    case 'TODO_EDITED':
+      return { ...state, editing: false, editingTitle: "", showSuccessSnackBar: true };
+    case 'SET_SUCCESS_SNACKBAR_VISIBILITY':
+      return { ...state, editing: false, editingTitle: "", showSuccessSnackBar: payload };
+    case 'SET_FAILURE_SNACKBAR_VISIBILITY':
+        return { ...state, editing: false, editingTitle: "", showFailureSnackBar: payload };
+    case 'HIDE_SUCCESS_SNACKBAR':
+      return { ...state, showSuccessSnackBar: false }
+    case 'HIDE_FAILURE_SNACKBAR':
+        return { ...state, showFailureSnackBar: false }
+    default:
+      throw new Error();
+  }
+}
 
 const theme = unstable_createMuiStrictModeTheme();
 
 function TodoItem({ todo }: { todo: iTodo }) {
-  const [editing, setEditing] = useState(false);
-  const [editingTitle, setEditingTitle] = useState("");
-  const [showSuccessSnackBar, setSuccessSnackbarVisibility] = useState(false);
-  const [showFailureSnackBar, setFailureSnackbarVisibility] = useState(false);
 
+  const [state, todoItemdispatch] = useReducer(reducer, initialState);
+  const { editing, editingTitle, showSuccessSnackBar, showFailureSnackBar } = state;
   const dispatch = useAppDispatch();
-  const deleteTodoHandler = () => {
-    dispatch(deleteTodo(todo));
-  };
-  const toggleTodoCompletedState = () => {
-    dispatch(editTodo({ ...todo, done: !todo.done }));
-  };
-  const editTodoHandler = () => {
-    setEditingTitle(todo.title);
-    setEditing(true);
-  };
-
-  const cancelTodoEditingHandler = () => {
-    setEditing(false);
-    setEditingTitle("");
-  };
 
   const [editTodoExecutor, { data, loading, error, reset }] = useMutation<iTodo>(editTodoAsync as any, editTodo);
   if (data) {
     reset && reset();
-    cancelTodoEditingHandler();
-    setSuccessSnackbarVisibility(true);
+    todoItemdispatch({ type: "TODO_EDITED" })
   }
 
   if (error) {
     reset && reset();
-    setFailureSnackbarVisibility(true);
-    // cancelTodoEditingHandler();
+    todoItemdispatch({ type: "SET_FAILURE_SNACKBAR_VISIBILITY", payload: true })
   }
-
 
   const saveTodoHandler = () => {
     editTodoExecutor({ ...todo, title: editingTitle });
   }
+
   const defaultActions = [
     <Button
       key="edit-action"
@@ -66,7 +88,7 @@ function TodoItem({ todo }: { todo: iTodo }) {
       color="secondary"
       className="action-btn"
       disabled={todo.done}
-      onClick={editTodoHandler}
+      onClick={() => todoItemdispatch({ type: "EDIT_TODO", payload: todo.title })}
     >
       <EditIcon fontSize="small" />
     </Button>,
@@ -75,7 +97,7 @@ function TodoItem({ todo }: { todo: iTodo }) {
       variant="outlined"
       color="error"
       className="action-btn"
-      onClick={deleteTodoHandler}
+      onClick={() => dispatch(deleteTodo(todo)) }
     >
       <DeleteIcon fontSize="small" color="warning" />
     </Button>,
@@ -100,7 +122,7 @@ function TodoItem({ todo }: { todo: iTodo }) {
       variant="outlined"
       color="primary"
       className="action-btn"
-      onClick={cancelTodoEditingHandler}
+      onClick={() => todoItemdispatch({ type: "CANCEL_EDITING" })}
     >
       Cancel
     </Button>,
@@ -113,7 +135,7 @@ function TodoItem({ todo }: { todo: iTodo }) {
         <Checkbox
           className="todo-checkbox"
           checked={todo.done}
-          onChange={toggleTodoCompletedState}
+          onChange={() => dispatch(editTodo({ ...todo, done: !todo.done }))}
         />
         <TextField
           id="outlined-basic"
@@ -122,14 +144,12 @@ function TodoItem({ todo }: { todo: iTodo }) {
           className="todo-input"
           size="small"
           variant="outlined"
-          onChange={(event: any) => setEditingTitle(event?.target.value)}
+          onChange={(event: any) => todoItemdispatch({ type: "SET_EDITING_TODO_TITLE", payload:event.target.value })}
           onKeyPress={(event: React.SyntheticEvent | Event) =>
             (event as any).key === "Enter" &&
             editingTitle && editingTitle.length > 0 &&
             saveTodoHandler()
           }
-          // onPointerOutCapture={editTodoHandler}
-          onPointerDownCapture={editTodoHandler}
         />
         {!editing ? defaultActions : editingActions}
       </div>
@@ -138,13 +158,13 @@ function TodoItem({ todo }: { todo: iTodo }) {
       <Snackbar
         anchorOrigin={{ vertical: "top", horizontal: "right" }}
         open={showSuccessSnackBar}
-        onClose={() => setSuccessSnackbarVisibility(false)}
+        onClose={() => todoItemdispatch({ type: "HIDE_SUCCESS_SNACKBAR" })}
         key={"top-right-success"}
         autoHideDuration={5000}
         title="Operation Successful!"
       >
         <MuiAlert
-          onClose={() => setSuccessSnackbarVisibility(false)}
+          onClose={() => todoItemdispatch({ type: "HIDE_SUCCESS_SNACKBAR" })}
           severity={"success"}
           sx={{ width: "100%" }}
         >
@@ -155,13 +175,13 @@ function TodoItem({ todo }: { todo: iTodo }) {
       <Snackbar
         anchorOrigin={{ vertical: "top", horizontal: "right" }}
         open={showFailureSnackBar}
-        onClose={() => setSuccessSnackbarVisibility(false)}
+        onClose={() => todoItemdispatch({ type: 'HIDE_FAILURE_SNACKBAR' })}
         key={"top-right-success"}
         autoHideDuration={1000}
-        title="Operation Successful!"
+        title="Operation Failed!"
       >
         <MuiAlert
-          onClose={() => setFailureSnackbarVisibility(false)}
+          onClose={() => todoItemdispatch({ type: 'HIDE_FAILURE_SNACKBAR' })}
           severity={"error"}
           sx={{ width: "100%" }}
         >
